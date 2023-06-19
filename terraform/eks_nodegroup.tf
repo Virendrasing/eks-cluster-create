63c3dd727fc810a38c5b718fc16e97ac9ae8852c
Virendrasing <108572348+Virendrasing@users.noreply.github.com>
2023-06-19 14:47:36 +0530

    Update eks_nodegroup.tf

--- terraform/eks_nodegroup.tf
+++ terraform/eks_nodegroup.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryRea
 }
 
 resource "aws_eks_node_group" "eks-nodegroup" {
-  cluster_name    = aws_eks_cluster.eks-cluster.name
+  cluster_name    = aws_eks_cluster.eks-demo-cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_noderole.arn
   subnet_ids      = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id ]
